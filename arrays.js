1388e3c361287f843a6cc96c14e2dbd667a50343
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-08 07:37:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 //var "foo" = mnm
 
 function addElementToBeginningOfArray (chocolateBars, mnm){
-  [mnm, ...chocolateBars]
-  return addElementToBeginningOfArray()
+  return [mnm, ...chocolateBars]
 }
