97110f74edf9999fa2314b41238f8d3b0048db8b
jlmissions@yahoo.com <jlmissions@yahoo.com>
2017-11-08 07:41:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray (chocolateBars, mnm){
   return [mnm, ...chocolateBars]
 }
+function destructivelyAddElementToBeginningOfArray (chocolateBars, mnm){
+  chocolateBars.unshift (mnm)
+  return chocolateBars
+}
